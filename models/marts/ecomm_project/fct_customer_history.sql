4df78dc88b5ebd42af0ea6a7c18ff7cc81780b0e
Keerthana Shanmugasundaram <keerthanasrishanmugam@gmail.com>
2026-06-10 09:24:00 +0000

    committed

---
+++ models/marts/ecomm_project/fct_customer_history.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='customer_id'
+    )
+}}
+
+select *,
+       dbt_valid_from AS start_date,
+       dbt_valid_to AS end_date,
+       case 
+           when dbt_valid_to is null
+           then 'Y'
+           else 'N'
+        end as current_flag
+        from {{ ref('snap_customer_history')}}

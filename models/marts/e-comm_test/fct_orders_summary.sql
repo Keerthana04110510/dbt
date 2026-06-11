c47a88c021d0ce9cd9cd8f0bccd86a0c49827f08
Keerthana Shanmugasundaram <keerthanasrishanmugam@gmail.com>
2026-06-11 09:30:50 +0000

    committed

--- models/marts/e-comm_test/fct_orders_summary.sql
+++ models/marts/e-comm_test/fct_orders_summary.sql
@@ -1,13 +1,14 @@
 {{
     config(
         materialized='incremental',
-        unique_key='order_id'
+        unique_key='order_id',
+        on_schema_change='sync_all_columns'
     )
 }}
 
 select order_id,
        price,
-       final_price,
        order_sk,
+       final_price,
        status
        from {{ ref('int_orders') }}

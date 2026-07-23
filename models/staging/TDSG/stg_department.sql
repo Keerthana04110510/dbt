a5fc65af5f1d8021d7eef4096d3647402bb78533
Keerthana Shanmugasundaram <keerthanasrishanmugam@gmail.com>
2026-07-23 05:36:51 +0000

    committ

--- models/staging/TDSG/stg_department.sql
+++ models/staging/TDSG/stg_department.sql
@@ -6,21 +6,13 @@
 
 SELECT
     COALESCE(TRY_CAST(spid AS NUMBER(38,0)), -1) AS spid,
-
     COALESCE(TRY_CAST(costcenterkey AS NUMBER(38,0)), -1) AS costcenterkey,
-
     COALESCE(NULLIF(TRIM(purchasinggroupkey), ''), 'UNKNOWN') AS purchasinggroupkey,
-
     COALESCE(TRY_CAST(activeflag AS NUMBER(38,0)), 0) AS activeflag,
-
     COALESCE(TRY_CAST(departmentkey AS NUMBER(38,0)), -1) AS departmentkey,
-
     COALESCE(TRY_CAST(divisionkey AS NUMBER(38,0)), -1) AS divisionkey,
-
     COALESCE(NULLIF(TRIM(departmentname), ''), 'UNKNOWN') AS departmentname,
-
     COALESCE(NULLIF(TRIM(departmenthead), ''), 'UNKNOWN') AS departmenthead,
-
     COALESCE(
         TRY_TO_DATE(createddate, 'DD-MM-YYYY HH24:MI'),
         TO_DATE('1900-01-01')
--- models/staging/banking/stg_accounts.sql
+++ models/staging/banking/stg_accounts.sql
@@ -2,6 +2,8 @@
              materialized='table'
     )    }}
 
+
+
 select
     updated_at,
     upper(account_number) as account_number,
@@ -9,4 +11,5 @@ select
     to_date(open_date, 'DD-MM-YYYY') as open_date,
     current_timestamp() as created_at
 from {{ source('stages_bank','bank_transactions') }}
+
 
